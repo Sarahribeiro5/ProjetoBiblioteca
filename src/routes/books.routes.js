5bc833ebdfa237cc48dc4423369e8401ec1d7c10
Sarahribeiro5 <sarahribeiro.tds2.senai@gmail.com>
2025-06-17 08:58:40 -0300

    feat: create books routes

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -6,5 +6,6 @@ const booksRouter = express.Router();
 
 // Rota para obter todos os livros
 booksRouter.get("/", booksController.getAllBooks);
+booksRouter.post("/", booksController.createBook);
 
 export default booksRouter;
